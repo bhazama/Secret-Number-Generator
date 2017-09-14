ab75564dd2aa74009d86d3b82ddc72c311837cae
Brendan Hazama <brendanhazama@Brendans-MacBook-Pro.local>
2017-09-13 14:25:03 -1000

    completed

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,10 @@
 'use strict';
 module.exports = function() {
-  
+  var randomNum;
+  return function secretNumber(){
+    if(randomNum === undefined){
+    randomNum = Math.random() * 1000000;
+   }
+    return randomNum;
+  };
 };
